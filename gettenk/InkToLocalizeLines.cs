c6f2187fe9e0fe992da1b996271e343217d43781
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2022-10-18 16:23:22 -0400

    the sources files. the beurocracy will follow! i miss those pascal days

---
+++ gettenk/InkToLocalizeLines.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ink;
+using Ink.Parsed;
+
+namespace gettenk
+{
+    public class LocalizedLine
+    {
+        public string line;
+        public List<Ink.Parsed.Object> objects = new List<Ink.Parsed.Object>();
+    }
+
+    public class InkToLocalizeLines
+    {
+        List<LocalizedLine> lines = new List<LocalizedLine>();
+
+        private void GatherSimple(Object obj)
+        {
+            if (obj == null) return;
+            if ((obj is Text t) && (!string.IsNullOrWhiteSpace(t.text)))
+            {
+                if (t.hasOwnDebugMetadata)
+                {
+                }
+            }
+            if (obj.content != null)
+            {
+                foreach (Ink.Parsed.Object sub in obj.content)
+                    WalkObj(sub, pfx + "  ");
+            }
+        }
+
+        public void GatherLines(Story parsedStory)
+        {
+            GatherSimple(parsedStory);
+        }
+    }
+}
---
+++ gettenk/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using Ink;
+using Ink.Runtime;
+using Ink.Parsed;
+
+namespace gettenk
+{
+    class Program
+    {
+
+        static void WalkObj(Ink.Parsed.Object obj, string pfx = "")
+        {
+            if (obj == null) return;
+            Console.Write(pfx);
+            Console.Write(obj.GetType().Name);
+            if ((obj is Text t) && (!string.IsNullOrWhiteSpace(t.text)))
+            {
+                Console.Write(" \"{0}\"", t.text);
+                if (t.hasOwnDebugMetadata)
+                {
+                    Console.Write(" (");
+                    Console.Write(t.debugMetadata.fileName);
+                    Console.Write(":");
+                    Console.Write(t.debugMetadata.startLineNumber);
+                    Console.Write(") *");
+                }
+            }
+            Console.WriteLine();
+            if (obj.content != null)
+            {
+                foreach (Ink.Parsed.Object sub in obj.content)
+                    WalkObj(sub, pfx + "  ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("please provide the input file name");
+                return;
+            }
+
+            string js = File.ReadAllText(args[0]);
+            InkParser p = new InkParser(js, args[0]);
+
+            try
+            {
+                Console.WriteLine("parsing...");
+                Ink.Parsed.Story flow = p.Parse();
+                Console.WriteLine("done!");
+                if (flow == null)
+                {
+                    Console.WriteLine("empty");
+                    return;
+                }
+                WalkObj(flow);
+
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+                Console.WriteLine(x.StackTrace);
+            }
+        }
+    }
+}
